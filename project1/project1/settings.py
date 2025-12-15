e02bfd21e6bff3f03f6d8905dc30e1f1a3aa37aa
Naveen44555 <naveennaveenyadav386@gmail.com>
2025-12-15 16:31:40 +0530

    allowed

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-dev-key-change-me")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aivenproject-1.onrender.com']
 
 
 # Application definition
